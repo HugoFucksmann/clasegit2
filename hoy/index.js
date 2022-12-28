3e7c5acf752f35a8e7e814eeed92148ccb6785b4
hugo.fuckmann <hugo.fuckman@errepar.com>
2022-12-28 20:20:00 -0300

    nuestro primer commit

---
+++ hoy/index.js
@@ -0,0 +1,109 @@
+//https://pokeapi.co/api/v2/pokemon?limit=10&offset=0&age=2
+
+const section = document.getElementById("section");
+
+class pokemon {
+  constructor(name) {
+    this.name = name;
+    this.entrenador = "juan";
+  }
+}
+
+async function getResult() {
+  const result = await fetch(
+    "https://pokeapi.co/api/v2/pokemon?limit=6&offset=0&age=2"
+  )
+    .then((res) => {
+      //console.log(res);
+      
+      return res.json();
+    })
+    .then((res) => {
+      console.log(res);
+      return res.results;
+    })
+    .catch((e) => {
+      console.warn("aqui el error");
+      console.warn(e);
+      return null;
+    });
+
+  console.log(result);
+
+  /*  for (let i = 0; i < result.length; i++) {
+    const cardPokemon = document.createElement("div");
+    const title = document.createElement("h4");
+    const value = document.createElement("h5");
+    const button = document.createElement("button");
+
+    title.textContent = "Nombre";
+    value.textContent = result[i].name;
+
+    button.textContent = "ver mas";
+
+    cardPokemon.classList.add("card");
+    cardPokemon.appendChild(title);
+    cardPokemon.appendChild(value);
+    cardPokemon.appendChild(button);
+    section.appendChild(cardPokemon);
+  } */
+  if(result !== null){
+    result.map(async (obj) => {
+      let cardPokemon = document.createElement("div");
+      let title = document.createElement("h4");
+      let value = document.createElement("h5");
+      const button = document.createElement("button");
+  
+      const datos = await fetch(obj.url)
+        .then((res) => res.json())
+        .catch((e) => console.log(e));
+  
+      console.log(datos);
+  
+      const newPokemon = new pokemon(obj.name);
+  
+      button.textContent = "ver mas";
+      title.textContent = "Nombre";
+      value.textContent = newPokemon.name;
+  
+      cardPokemon.classList.add("card");
+      cardPokemon.appendChild(title);
+      cardPokemon.appendChild(value);
+      cardPokemon.appendChild(button);
+      section.appendChild(cardPokemon);
+    });
+  }
+
+}
+
+function appendH1(){
+
+  try {
+     
+    const storage = localStorage.getItaem("key")
+    console.log(storage);
+  } catch(e) {
+    console.warn("ocurrio un error en appendH1");
+    console.warn(e);
+  }
+
+ 
+
+
+}
+
+function cortarTexto(text){
+  try {
+
+    return text.slice(0,3)
+
+    
+  } catch (error) {
+    console.log(error);
+    return "ocurrio un error"
+  }
+}
+
+
+console.log(cortarTexto(2134214));
+getResult();
---
+++ index.js
@@ -0,0 +1,42 @@
+let h4 = document.getElementById("name");
+let ul = document.getElementById("listMoves");
+
+async function getPikachu() {
+  const pikachu = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    .then((res) => {
+      console.log(res);
+
+      if (res.status !== 200) throw Error;
+
+      return res.json();
+    })
+    .catch((e) => {
+      console.warn("El error es: ", e);
+      return null;
+    })
+    .finally(() => alert("la promesa termino"));
+
+  console.log(pikachu);
+
+  if (pikachu !== null) {
+    for (i = 0; i < 10; i++) {
+      let li = document.createElement("li");
+      li.textContent = pikachu.moves[i].move.name;
+      ul.appendChild(li);
+    }
+  }
+}
+
+async function getAllPokemons(name, age, evolution) {
+  const pokemons = await fetch(
+    "https://pokeapi.co/api/v2/pokemon?limit=12&offset=0"
+  )
+    .then((res) => res.json())
+    .catch((e) => console.log(e));
+
+  console.log(pokemons);
+}
+
+getPikachu();
+getAllPokemons();
+console.log("HOLA MUNDO");
